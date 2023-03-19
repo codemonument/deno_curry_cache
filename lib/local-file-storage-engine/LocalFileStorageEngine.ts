e06e82e2cd094df63d2398bd4c93430cc5fcc662
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-19 17:26:27 +0100

    move LocalFileStorageEngineOptions into LocalFileStorageEngine

--- lib/local-file-storage-engine/LocalFileStorageEngine.ts
+++ lib/local-file-storage-engine/LocalFileStorageEngine.ts
@@ -1,6 +1,22 @@
 import { CurryCacheStorageEngine } from "../types/CurryCacheStorageEngine.ts";
-import { LocalFileStorageEngineOptions } from "./LocalFileStorageEngineOptions.ts";
-import { join } from "../../dependencies/std-path.ts";
+import { join } from "../../deps/std.ts";
+
+export interface LocalFileStorageEngineOptions {
+  type: "text" | "binary";
+
+  /**
+   * The folder where cache files with auto-generated names will be saved
+   * Can be overwritten by forceCacheFilePath.
+   */
+  cacheBasePath?: string;
+
+  /**
+   * The exact file path (down to the file ending) where the cache file gets written to.
+   * If not set, it will be auto-generated from the function name curried by curryCache.
+   */
+  forceCacheFilePath?: string;
+}
+
 
 export class LocalFileStorageEngine
   implements CurryCacheStorageEngine<LocalFileStorageEngineOptions> {
