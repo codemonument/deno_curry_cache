ec4cbce5e9b766bc6840334484cb9c7e0770e643
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-19 17:32:05 +0100

    deactivate export check for LcoalJsonDbStorageEngine, bc.it's work in progress

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -1,10 +1,12 @@
 import { assert, describe, it } from "std_testing";
-import { curryCache, LocalFileStorageEngine, LocalJsonDbStorageEngine } from "@/mod.ts";
+import { curryCache, LocalFileStorageEngine } from "@/mod.ts";
 
 describe(`mod.ts`, () => {
   it(`should export correct objects and types`, () => {
     assert(curryCache);
     assert(LocalFileStorageEngine);
-    assert(LocalJsonDbStorageEngine);
+
+    // WIP
+    // assert(LocalJsonDbStorageEngine);
   });
 });
