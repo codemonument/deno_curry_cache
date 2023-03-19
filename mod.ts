6b2258569d0f756f894b603afac2d63a751f795b
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-19 17:23:21 +0100

    export and test export for the two storage engines

--- mod.ts
+++ mod.ts
@@ -4,3 +4,5 @@
  */
 
 export * from "./lib/curry_cache.ts"
+export * from "./lib/local-file-storage-engine/LocalFileStorageEngine.ts"
+export * from "./lib/local-json-db-storage-engine/LocalJsonDbStorageEngine.ts"
--- test/mod.test.ts
+++ test/mod.test.ts
@@ -1,8 +1,10 @@
 import { assert, describe, it } from "std_testing";
-import { dummyExport } from "@/mod.ts";
+import { curryCache, LocalFileStorageEngine, LocalJsonDbStorageEngine } from "@/mod.ts";
 
 describe(`mod.ts`, () => {
   it(`should export correct objects and types`, () => {
-    assert(dummyExport);
+    assert(curryCache);
+    assert(LocalFileStorageEngine);
+    assert(LocalJsonDbStorageEngine);
   });
 });
