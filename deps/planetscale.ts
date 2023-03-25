03967a86e7aa2effd4eff6c96f9a6b3b9d39ae08
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-25 01:09:45 +0100

    switch from kysely with planetscale to plain planetscale http driver (due to dynamic table name for caching table!)

--- deps/planetscale.ts
+++ deps/planetscale.ts
@@ -1,3 +1,5 @@
-export * from "https://esm.sh/v110/kysely@0.23.5";
-export * from "https://esm.sh/v110/kysely-planetscale@1.3.0";
+// export * from "https://esm.sh/v110/kysely@0.23.5";
+// export * from "https://esm.sh/v110/kysely-planetscale@1.3.0";
 // export * from "https://esm.sh/v110/kysely-planetscale@1.3.0?external=kysely&deps=@planetscale/database@1.6.0"
+
+export * from "https://esm.sh/v110/@planetscale/database@1.6.0";
--- test/deps_planetscale.test.ts
+++ test/deps_planetscale.test.ts
@@ -2,10 +2,9 @@
  * Tests the "importability" for all dependencies needed for planetscale
  */
 
-import { Kysely, PlanetScaleDialect } from "@/deps/planetscale.ts";
+import { connect } from "@/deps/planetscale.ts";
 import { assert } from "std_testing";
 
-Deno.test(`Testing "importability" of kysely and it's planetscale adapter`, () => {
-  assert(Kysely);
-  assert(PlanetScaleDialect);
+Deno.test(`Testing "importability" of planetscale http driver`, () => {
+  assert(connect);
 });
