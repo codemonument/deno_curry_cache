d5903a6e0ceba08e2c8f511e10de6822093ba67a
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-25 02:27:30 +0100

    fix connection with planetscale by using another env var loader!

--- test/deps_planetscale.test.ts
+++ test/deps_planetscale.test.ts
@@ -1,10 +1,33 @@
 /**
  * Tests the "importability" for all dependencies needed for planetscale
  */
+import { load } from "envy";
 
 import { connect } from "@/deps/planetscale.ts";
-import { assert } from "std_testing";
+import { assert, assertEquals, assertStrictEquals } from "std_testing";
+
+const env = await load();
 
 Deno.test(`Testing "importability" of planetscale http driver`, () => {
   assert(connect);
+});
+
+const host = env["PLANETSCALE_HOST"] ?? "";
+const username = env["PLANETSCALE_USER"] ?? "";
+const password = env["PLANETSCALE_PASSWD"] ?? "";
+
+Deno.test({
+  name: `Testing connection to planetscale`,
+  ignore: host === "" || username === "" || password === "",
+  fn: async () => {
+    const config = {
+      host,
+      username,
+      password,
+    };
+
+    const db = connect(config);
+    const results = await db.execute("select 1 from dual where 1=?", [1]);
+    console.log(results);
+  },
 });
